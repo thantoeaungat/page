3aa657d7073cc139d0422090ca92b4933e6e1f91
Than Toe Aung <thantoeaung@live.com>
2026-03-22 22:30:21 +0630

    update

--- apidriver/ApkShellext2_Channel_TTA/ApkShellext2_Channel_TTA/restart_explorer.bat
+++
@@ -1,7 +0,0 @@
-REM CHOICE /M "Restart explorer process?"
-REM IF %ERRORLEVEL%==1 (
-  TASKKILL /F /IM explorer.exe
-  TASKKILL /F /IM dllhost.exe
-  REM pause
-  START %WINDIR%\explorer.exe
-REM )
--- apidriver/uac/uac.bat
+++
@@ -1,1 +0,0 @@
-C:\Windows\System32\cmd.exe /k %windir%\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
